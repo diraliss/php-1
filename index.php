2ca1b14f7aeb0eab1131f53d8c20eaf68740fd45
Maria Gubanova <diraliss@ya.ru>
2018-01-20 22:00:22 +0300

    lesson 3

--- index.php
+++ index.php
@@ -1,145 +1,206 @@
 <?php
-function sum($arg1, $arg2)
-{
-    return ($arg1 + $arg2);
-}
+header("content-type: text/html; charset=utf-8");
 
-function sub($arg1, $arg2)
-{
-    return ($arg1 - $arg2);
-}
+$dictionary = [
+    "Адыгея Республика" => [
+        "Адыгейск",
+        "Майкоп",
+    ],
+    "Бурятия Республика" => [
+        "Бабушкин",
+        "Гусиноозерск",
+        "Закаменск",
+        "Кяхта",
+        "Северобайкальск",
+        "Улан-Удэ",
+    ],
+    "Алтай Республика" => [
+        "Горно-Алтайск",
+    ],
+    "Ингушетия Республика" => [
+        "Карабулак",
+        "Магас",
+        "Малгобек",
+        "Назрань",
+    ],
+    "Архангельская Область" => [
+        "Архангельск",
+        "Вельск",
+        "Каргополь",
+        "Коряжма",
+        "Котлас",
+        "Мезень",
+        "Мирный",
+        "Новодвинск",
+        "Няндома",
+        "Онега",
+    ],
+];
 
-function mul($arg1, $arg2)
-{
-    return ($arg1 * $arg2);
-}
+$menu = [
+    [
+        "title" => "Example 1",
+        "url" => "",
+        "childes" => [
+            [
+                "title" => "Example 1.1",
+                "url" => "",
+                "childes" => []
+            ],
+            [
+                "title" => "Example 1.2",
+                "url" => "",
+                "childes" => [
+                    [
+                        "title" => "Example 1.2.1",
+                        "url" => "",
+                        "childes" => []
+                    ],
+                    [
+                        "title" => "Example 1.2.2",
+                        "url" => "",
+                        "childes" => []
+                    ],
+                ]
+            ],
+            [
+                "title" => "Example 1.3",
+                "url" => "",
+                "childes" => []
+            ],
+        ]
+    ],
+    [
+        "title" => "Example 2",
+        "url" => "",
+        "childes" => [
+            [
+                "title" => "Example 2.1",
+                "url" => "",
+                "childes" => []
+            ],
+            [
+                "title" => "Example 2.2",
+                "url" => "",
+                "childes" => []
+            ],
+        ]
+    ],
+    [
+        "title" => "Example 3",
+        "url" => "",
+        "childes" => []
+    ],
+    [
+        "title" => "Example 4",
+        "url" => "",
+        "childes" => []
+    ],
+];
 
-function div($arg1, $arg2)
+function ex1()
 {
-    return ($arg1 / $arg2);
-}
-
-function _diff()
-{
-    $a = rand(-20, 20);
-    $b = rand(-20, 20);
-
-    if ($a >= 0 && $b >= 0) {
-        return "$a - $b = " . sub($a, $b);
-    } elseif ($a < 0 && $b < 0) {
-        return "$a * $b = " . mul($a, $b);
-    } else {
-        return "$a + $b = " . sum($a, $b);
+    $conter = 0;
+    $result = [];
+    while ($conter <= 100) {
+        if ($conter % 3 == 0) $result[] = $conter;
+        $conter++;
     }
+    return implode(", ", $result);
 }
 
-function _rand()
+function ex2()
 {
-    $a = rand(0, 15);
-    $a_arr = [];
-    switch ($a) {
-        case 0:
-            $a_arr[] = 0;
-        case 1:
-            $a_arr[] = 1;
-        case 2:
-            $a_arr[] = 2;
-        case 3:
-            $a_arr[] = 3;
-        case 4:
-            $a_arr[] = 4;
-        case 5:
-            $a_arr[] = 5;
-        case 6:
-            $a_arr[] = 6;
-        case 7:
-            $a_arr[] = 7;
-        case 8:
-            $a_arr[] = 8;
-        case 9:
-            $a_arr[] = 9;
-        case 10:
-            $a_arr[] = 10;
-        case 11:
-            $a_arr[] = 11;
-        case 12:
-            $a_arr[] = 12;
-        case 13:
-            $a_arr[] = 13;
-        case 14:
-            $a_arr[] = 14;
-        case 15:
-            $a_arr[] = 15;
-    }
-    return implode("<br>", $a_arr);
+    $conter = 0;
+    $result = [];
+    do {
+        if ($conter == 0) {
+            $result[] = "$conter – это ноль.";
+        } elseif ($conter % 2 == 0) {
+            $result[] = "$conter – четное число.";
+        } else {
+            $result[] = "$conter – нечетное число.";
+        }
+        $conter++;
+    } while ($conter <= 10);
+    return implode("<br>", $result);
 }
 
-function operation($arg1, $arg2, $operation)
+function ex3($first = "")
 {
-    switch ($operation) {
-        case "-":
-            return sub($arg1, $arg2);
-            break;
-        case "+":
-            return sum($arg1, $arg2);
-            break;
-        case "*":
-            return mul($arg1, $arg2);
-            break;
-        case "/":
-            return div($arg1, $arg2);
-            break;
+    global $dictionary;
+    $result = [];
+    foreach ($dictionary as $region => $cityArr) {
+        if ($first !== "") {
+            $newCityArr = [];
+            foreach ($cityArr as $city) {
+                if (preg_split("//u", $city)[1] == $first) $newCityArr[] = $city;
+            }
+            $cityArr = $newCityArr;
+        }
+        $result[] = "$region:<br>" . implode(", ", $cityArr);
     }
-    return 0;
+    return implode("<br>", $result);
 }
 
-function _pow($val, $pow)
+function transliterate($string)
 {
-    if ($pow != 0) {
-        return $val * _pow($val, $pow - 1);
-    } elseif ($val != 0) {
-        return 1;
-    } else {
-        return 0;
-    }
+    $russian = array("А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я", "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", " ");
+    $english = array("A", "B", "V", "G", "D", "E", "E", "Gh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H", "C", "Ch", "Sh", "Sch", "Y", "Y", "Y", "E", "Yu", "Ya", "a", "b", "v", "g", "d", "e", "e", "gh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c", "ch", "sh", "sch", "\"", "y", "'", "e", "yu", "ya", "_");
+
+    return str_replace($russian, $english, $string);
 }
 
-function power($val, $pow)
+function menu($menu)
 {
-    if ($pow < 0) {
-        return 1 / _pow($val, abs($pow));
-    } else {
-        return _pow($val, abs($pow));
+    $result = "<ul>";
+    foreach ($menu as $elem) {
+        $title = $elem["title"];
+        $url = $elem["url"];
+        $result .= "<li><a href='$url'>$title</a>";
+        if (count($elem["childes"]) > 0) {
+            $result .= menu($elem["childes"]);
+        }
+        $result .= "</li>";
     }
+    $result .= "</ul>";
+    return $result;
 }
 
-date_default_timezone_set('Europe/Moscow');
-header('content-type: text/html; charset=utf-8');
+function ex7()
+{
+    $result = "";
+    for ($i = 0; $i <= 9; $result .= $i++ . "; ") ;
+    return $result;
+}
 
-$rand_operation = ["+", "-", "*", "/"][rand(0, 3)];
 
-$title = 'Hello world!';
-$year = date('Y');
-$result_diff = _diff();
-$result_rand = _rand();
-$result_operation = operation(rand(-20, 20), rand(-20, 20), $rand_operation);
-$result_power = power(rand(-5, 5), rand(-5, 5));
+$ex1 = ex1();
+$ex2 = ex2();
+$ex3 = ex3();
+$transliterate = transliterate("Меня зовут Губанова Мария Михайловна. Рада приветствовать!");
+$ex6 = menu($menu);
+$ex7 = ex7();
+$ex8 = ex3("К");
 
 $content = "
 <html>
-<header>
-<title>$title</title>
-</header>
+<header></header>
 <body>
 <p>Ex 1</p>
-<p>$result_diff</p>
+<p>$ex1</p>
 <p>Ex 2</p>
-<p>$result_rand</p>
-<p>Ex 3-4</p>
-<p>$result_operation</p>
+<p>$ex2</p>
+<p>Ex 3</p>
+<p>$ex3</p>
+<p>Ex 4, 5, 9</p>
+<p>$transliterate</p>
 <p>Ex 6</p>
-<p>$result_power</p>
-<footer>Ex 5: $year</footer>
+<p>$ex6</p>
+<p>Ex 7</p>
+<p>$ex7</p>
+<p>Ex 8</p>
+<p>$ex8</p>
 </body>
 </html>
 ";
