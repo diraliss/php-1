df3a5799036a1a2c0d9482a9b1e656a6641c02a3
Maria Gubanova <diraliss@ya.ru>
2018-01-20 21:55:03 +0300

    lesson 2

--- index.php
+++ index.php
@@ -1,10 +1,129 @@
 <?php
+function sum($arg1, $arg2)
+{
+    return ($arg1 + $arg2);
+}
+
+function sub($arg1, $arg2)
+{
+    return ($arg1 - $arg2);
+}
+
+function mul($arg1, $arg2)
+{
+    return ($arg1 * $arg2);
+}
+
+function div($arg1, $arg2)
+{
+    return ($arg1 / $arg2);
+}
+
+function _diff()
+{
+    $a = rand(-20, 20);
+    $b = rand(-20, 20);
+
+    if ($a >= 0 && $b >= 0) {
+        return "$a - $b = " . sub($a, $b);
+    } elseif ($a < 0 && $b < 0) {
+        return "$a * $b = " . mul($a, $b);
+    } else {
+        return "$a + $b = " . sum($a, $b);
+    }
+}
+
+function _rand()
+{
+    $a = rand(0, 15);
+    $a_arr = [];
+    switch ($a) {
+        case 0:
+            $a_arr[] = 0;
+        case 1:
+            $a_arr[] = 1;
+        case 2:
+            $a_arr[] = 2;
+        case 3:
+            $a_arr[] = 3;
+        case 4:
+            $a_arr[] = 4;
+        case 5:
+            $a_arr[] = 5;
+        case 6:
+            $a_arr[] = 6;
+        case 7:
+            $a_arr[] = 7;
+        case 8:
+            $a_arr[] = 8;
+        case 9:
+            $a_arr[] = 9;
+        case 10:
+            $a_arr[] = 10;
+        case 11:
+            $a_arr[] = 11;
+        case 12:
+            $a_arr[] = 12;
+        case 13:
+            $a_arr[] = 13;
+        case 14:
+            $a_arr[] = 14;
+        case 15:
+            $a_arr[] = 15;
+    }
+    return implode("<br>", $a_arr);
+}
+
+function operation($arg1, $arg2, $operation)
+{
+    switch ($operation) {
+        case "-":
+            return sub($arg1, $arg2);
+            break;
+        case "+":
+            return sum($arg1, $arg2);
+            break;
+        case "*":
+            return mul($arg1, $arg2);
+            break;
+        case "/":
+            return div($arg1, $arg2);
+            break;
+    }
+    return 0;
+}
+
+function _pow($val, $pow)
+{
+    if ($pow != 0) {
+        return $val * _pow($val, $pow - 1);
+    } elseif ($val != 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function power($val, $pow)
+{
+    if ($pow < 0) {
+        return 1 / _pow($val, abs($pow));
+    } else {
+        return _pow($val, abs($pow));
+    }
+}
+
 date_default_timezone_set('Europe/Moscow');
 header('content-type: text/html; charset=utf-8');
 
-$h1 = 'Привет, мир!';
+$rand_operation = ["+", "-", "*", "/"][rand(0, 3)];
+
 $title = 'Hello world!';
 $year = date('Y');
+$result_diff = _diff();
+$result_rand = _rand();
+$result_operation = operation(rand(-20, 20), rand(-20, 20), $rand_operation);
+$result_power = power(rand(-5, 5), rand(-5, 5));
 
 $content = "
 <html>
@@ -12,38 +131,18 @@ $content = "
 <title>$title</title>
 </header>
 <body>
-<h1>$h1</h1>
-<p>$year</p>
+<p>Ex 1</p>
+<p>$result_diff</p>
+<p>Ex 2</p>
+<p>$result_rand</p>
+<p>Ex 3-4</p>
+<p>$result_operation</p>
+<p>Ex 6</p>
+<p>$result_power</p>
+<footer>Ex 5: $year</footer>
 </body>
 </html>
 ";
 
 echo $content;
-
-echo "<hr>";
-
-$a = 5;
-$b = '05';
-
-echo "<p>\$a == \$b => ";
-var_dump($a == $b);
-echo "</p><p>Так как используется не тождественное сравнение. При таком сравнении числа со строкой, строка будет преобразована в число</p>";
-
-echo "<p>(int)'012345' => ";
-var_dump((int)'012345');
-echo "</p><p>Так как (int) преобразует тип строки в число</p>";
-
-echo "<p>(float)123.0 === (int)123.0) => ";
-var_dump((float)123.0 === (int)123.0);
-echo "</p><p>Так как используется тождественное сравнение и по значению, и по типу</p>";
-
-echo "<p>(int)0 === (int)'hello, world') => ";
-var_dump((int)0 === (int)'hello, world');
-echo "</p><p>После преобразования 'hello, world' в число оно будет равно 0, так как строка чисел в себе не содержит</p>";
-
-echo "<hr>";
-
-echo "<p>\$a = $a; \$b = $b; <br>";
-list($b, $a) = array($a, $b);
-echo "<p>\$a = $a; \$b = $b;";
 ?>
